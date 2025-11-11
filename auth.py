ee566f7dd64471b459deed7eab38e64b19393fc5
salman-bokhari <salman.bokhari@eurustechnologies.com>
2025-11-11 07:21:50 +0500

    Testing

--- auth.py
+++ auth.py
@@ -3,7 +3,10 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    # Truncate to 72 bytes for bcrypt
+    password_bytes = password.encode("utf-8")[:72]
+    return pwd_context.hash(password_bytes)
+
 
 def verify_password(password: str, hashed: str) -> bool:
     return pwd_context.verify(password, hashed)
